d2b9581f15e5ac10eaf4d566ab4cf797ff118f73
VioletXF <zudevxf@gmail.com>
2026-06-27 18:58:37 +0900

    Share BMS search hash normalization

--- src/bms_search/Internal.h
+++ src/bms_search/Internal.h
@@ -111,6 +111,7 @@ struct PackageSourceLookupResult {
 
 std::string trimCopy(const std::string &value);
 std::string lowerCopy(std::string value);
+std::string normalizedHash(const std::string &value);
 bool endsWith(std::string_view value, std::string_view suffix);
 bool hostMatches(const std::string &host, std::string_view domain);
 std::string replaceAll(std::string value, std::string_view needle,
